2b76ab34520d723a4a435b74afe85d428772ef22
akino <akikoba2001@gmail.com>
2021-11-28 20:39:30 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `StudyBook`,
+    description: `Webチームで学ぶことを心をこめて書いたレポジトリ`,
+    author: `@akinori`,
+    siteUrl: `https://https://org.wasedasai.net/StudyBook/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
