48585eb53d9647b6ddd0439324936d786f3879f7
akino <akikoba2001@gmail.com>
2021-11-30 01:50:20 +0900

    Updates

---
+++ component---src-pages-404-js-789f14e9df73db808636.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(t,e,n){n.r(e);var u=n(7294),a=n(2480),l=n(3751);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-789f14e9df73db808636.js.map
